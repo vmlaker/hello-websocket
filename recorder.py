43e3d047269804f4ecea6ba492d94f7a93886624
vmlaker <velimir.mlaker@gmail.com>
2014-01-10 16:57:38 -0800

    Add optional video dimensions, add comments.

--- recorder.py
+++ recorder.py
@@ -1,5 +1,9 @@
 """
 Run webcam capture and maintain current image on socket server.
+
+Usage:
+
+   python recorder.py port [width] [height]
 """
 
 # Import standard modules.
@@ -12,9 +16,16 @@ import cv2
 import numpy
 from coils import MapSockClient, MapSockRequest
 
+# Create video capture object and socket client.
 cap = cv2.VideoCapture(-1)
 map_client = MapSockClient('localhost', int(sys.argv[1]), encode=False)
 
+# Set video dimensions, if given.
+if len(sys.argv) > 2: cap.set(3, int(sys.argv[2]))
+if len(sys.argv) > 3: cap.set(4, int(sys.argv[3]))
+
+# Repeatedly capture current image, 
+# encode and push on socket connection.
 while True:
     hello, image = cap.read()
     hello, image = cv2.imencode('.jpg', image)
